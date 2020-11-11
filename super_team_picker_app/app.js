c209fad093e0488337dc21167f28a88145294e19
Chandanpreet91 <kaurchandanpreet91@gmail.com>
2020-11-10 16:56:22 -0800

    Add logger, cookie-parser,method-override,express-static, express.ulencoded middleware

--- super_team_picker_app/app.js
+++ super_team_picker_app/app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const logger = require('morgan')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const methodOverride = require('method-override');
+const { request } = require('http');
+const { response } = require('express');
 
 const app = express()
+app.set('view engine',"ejs");
+app.use(express.static(path.join(__dirname, "public")))
+app.use(logger("dev"));
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(
+    methodOverride((req,res) => {
+        if (req.body && typeof req.body === "object" && "_method" in req.body){
+            const method = req.body._method;
+            delete req.body._method;
+            return method;
+        }
+    })
+)
 
 const PORT = 3000
 const ADDRESS = 'localhost';
